317eae1aa5da51297232dac4c9e0ce9f0a052958
Oleg <bulgakov.exeedteam@gmail.com>
2019-07-10 14:20:38 +0300

    first commi7

--- app.js
+++ app.js
@@ -1,17 +1,31 @@
-const port = process.env.PORT || 3000
+// const port = process.env.PORT || 3000
 const express = require('express');
 const mongoose = require('mongoose');
+
+const dev_db_url = 'mongodb://someuser:abcd1234@ds123619.mlab.com:23619/productstutorial';
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
-mongoose.connect('mongodb://localhost:27017/todo');
 require('./models/task.model');
 
 require('./routes/task.route')(app)
 
-app.listen(port,() => {
+const port = 3000;
+app.listen(port, () => {
     console.log(`Server running at port `+port);
-  });
+});
+
+
+
+
+
+
